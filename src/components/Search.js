f647a6abe3b4095ccbe85dfc11155519eb63b8dc
Zachary Youngdale <zyoungdale@gmail.com>
2022-09-28 10:00:46 -0400

    commit tree 703a5fc869ee26e63f2fc85dc5704302675d223a

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Search() {
+function Search({ setSearch, search, searchListings }) {
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    searchListings();
   }
 
   return (
@@ -12,8 +12,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
